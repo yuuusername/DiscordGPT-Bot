b0375a3638e26d6b2037bcda0f2df1f517c182a4
Yuuusername <72343301+Yuuusername@users.noreply.github.com>
2023-03-07 00:10:21 +1100

    fix: change OpenAI request body default values.
    Make the request body default values more natural.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -74,10 +74,11 @@ module.exports = {
 				let completion = await openai.createChatCompletion({
 					model: "gpt-3.5-turbo",
 					messages: conversation,
-					temperature: 1.1,
-					top_p: 0.1,
-					max_tokens: 200,
-					frequency_penalty: 1
+					temperature: 0,
+					top_p: 0,
+					max_tokens: 256,
+					frequency_penalty: 0,
+					presence_penalty: 0,
 				});
 
 				let response = completion.data.choices[0].message;
