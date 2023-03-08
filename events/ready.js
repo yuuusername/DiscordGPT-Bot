c9df21808e73eb64e15c67869a30166ba6f16aa0
Yuuusername <72343301+Yuuusername@users.noreply.github.com>
2023-03-09 00:30:39 +1100

    feat: Add invite link for bot
    For ease of use, an invite link for the bot is auto-generated.

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Need to invite your bot? Here you go: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=274877991936&scope=bot`)
 	},
 };
